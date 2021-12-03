ad9c3544f6df24265a2d75086e1d3f1c60d57ad9
yhr <1614764137@qq.com>
2021-12-03 21:39:47 +0800

    first commit

--- src/main/java/com/yhr/git/GitTest.java
+++ src/main/java/com/yhr/git/GitTest.java
@@ -3,5 +3,6 @@ package com.yhr.git;
 public class GitTest{
     public static void main(String[] args) {
         System.out.println("hello yhr!");
+        System.out.println("hello yhrrr!");
     }
 }
